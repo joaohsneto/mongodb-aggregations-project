e64bbc1c05ac3061b43ee064343620cd2add688c
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-08-28 07:46:56 -0300

    Resolvido o desafio 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,19 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $gt: new Date("2016-03-09T23:59:59Z"), $lt: new Date("2016-03-11T00:00:00Z") },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      mediaTime: { $avg: { $substract: ["$stopTime", "$startTime"] } },
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      duracaoMediaEmMinutos: { $ceil: { $divide: ["$mediaTime", 60000] } },
+    },
+  },
+]);
